c39b65bce8673bd8734e67e42e0ebb04f323969b
Ilyes Hammadi <ilyes.hammadi@tiqets.com>
2022-11-28 23:31:40 +0100

    feat: Add TCP port

--- app/server.go
+++ app/server.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	// Uncomment this block to pass the first stage
-	// "net"
-	// "os"
+	"net"
+	"os"
 )
 
 func main() {
@@ -13,14 +13,14 @@ func main() {
 
 	// Uncomment this block to pass the first stage
 	//
-	// l, err := net.Listen("tcp", "0.0.0.0:6379")
-	// if err != nil {
-	// 	fmt.Println("Failed to bind to port 6379")
-	// 	os.Exit(1)
-	// }
-	// _, err = l.Accept()
-	// if err != nil {
-	// 	fmt.Println("Error accepting connection: ", err.Error())
-	// 	os.Exit(1)
-	// }
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	if err != nil {
+		fmt.Println("Failed to bind to port 6379")
+		os.Exit(1)
+	}
+	_, err = l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
 }
